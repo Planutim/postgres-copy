45fb67dfe770427308983eb12cff08e8ffac0a71
Sanzhar <danceontheair@gmail.com>
2021-02-23 18:02:24 +0600

    firstcopy

---
+++ api/controllers/base.go
@@ -0,0 +1,53 @@
+package controllers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	"github.com/planutim/postgres-copy/api/models"
+
+	"github.com/jinzhu/gorm/dialects/postgres"
+)
+
+type Server struct {
+	DB     *gorm.DB
+	Router *mux.Router
+}
+
+func (server *Server) Initialize(DbDriver, DbUser, DbPassword, DbPort, DbName, DbHost string) {
+	var err error
+	if DbDriver == "mysql" {
+		DBURL := fmt.Sprintf("%s:%s:@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DbUser, DbPassword, DbHost, DbPort, DbName)
+		server.DB, err = gorm.Open(DbDriver, DBURL)
+		if err != nil {
+			fmt.Printf("Cannot connect to %s database", DbDriver)
+			log.Fatal("This is the error:", err)
+		} else {
+			fmt.Printf("We are connected to the %s database", DbDriver)
+		}
+	}
+	if DbDriver == "postgres" {
+		DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
+		server.DB, err = gorm.Open(DbDriver, DBURL)
+		if err != nil {
+			fmt.Printf("Cannot connect to %s database", DbDriver)
+			log.Fatal("This is the error:", err)
+		} else {
+			fmt.Printf("We are connected to the %s database", DbDriver)
+		}
+	}
+
+	server.DB.Debug().AutoMigrate(&models.User{}, &models.Post{}) // database migration
+
+	server.Router = mux.NewRouter()
+
+	server.initializeRoutes()
+}
+
+func (server *Server) Run(addr string) {
+	fmt.Printf("Listening to port %s", addr)
+	log.Fatal(http.ListenAndServe(addr, server.Router))
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/planutim/postgres-copy
+
+go 1.16
+
+require (
+	github.com/badoux/checkmail v1.2.1 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/mattn/go-sqlite3 v2.0.1+incompatible // indirect
+	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
+)
